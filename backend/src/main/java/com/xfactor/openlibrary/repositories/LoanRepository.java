716a4d997f40254c14459dd01d0b8db3b5de7f7a
Sourayam Sam <108325634+souryamsam@users.noreply.github.com>
2023-04-16 15:43:21 +0530

    Update LoanRepository.java

---
+++ backend/src/main/java/com/xfactor/openlibrary/repositories/LoanRepository.java
@@ -0,0 +1,15 @@
+package com.xfactor.openlibrary.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xfactor.openlibrary.domain.Loan;
+
+public interface LoanRepository extends JpaRepository<Loan, Long>{
+    List<Loan> findByCheckoutDate(String checkoutDate);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
